75eb4b477a8cbd3c2ae78ae35358173cd989252d
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-11-29 18:33:57 +0200

    Add final class APIClient

---
+++ BabylonHealthDemo/APIClient.swift
@@ -0,0 +1,48 @@
+//
+//  APIClient.swift
+//  BabylonHealthDemo
+//
+//  Created by Michail Zarmakoupis on 29/11/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import Foundation
+
+enum APIClientResult {
+    case success(Data)
+    case error(APIClientError)
+}
+
+enum APIClientError {
+    case badRequest
+    case server
+    case unknown
+}
+
+final class APIClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func execute(request: URLRequest, completion: @escaping (APIClientResult) -> Void) {
+        session.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse {
+                if let data = data, response.statusCode > 199, response.statusCode < 300 {
+                    completion(.success(data))
+                } else if response.statusCode > 399 && response.statusCode < 500 {
+                    completion(.error(.badRequest))
+                } else if response.statusCode > 499 && response.statusCode < 600 {
+                    completion(.error(.server))
+                } else {
+                    completion(.error(.unknown))
+                }
+            } else {
+                completion(.error(.unknown))
+            }
+            }
+            .resume()
+    }
+}

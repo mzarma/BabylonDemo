15437ce80a95db675d000a240cda92c1b11f9ac9
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-11-29 19:24:23 +0200

    Add PostsViewController

---
+++ BabylonHealthDemo/PostsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PostsViewController.swift
+//  BabylonHealthDemo
+//
+//  Created by Michail Zarmakoupis on 29/11/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import UIKit
+
+final class PostsViewController: UIViewController {
+    private let tableView = UITableView(frame: .zero)
+    
+    private var dataSource: UITableViewDataSource!
+    private var delegate: UITableViewDelegate!
+    
+    convenience init(dataSource: UITableViewDataSource, delegate: UITableViewDelegate) {
+        self.init()
+        self.dataSource = dataSource
+        self.delegate = delegate
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.dataSource = dataSource
+        tableView.delegate = delegate
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}

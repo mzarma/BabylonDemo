335cf670994122be036aa3a474a8937c222f3173
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-11-30 11:42:52 +0200

    Add URLRequest Factory

---
+++ BabylonHealthDemo/URLRequestFactory.swift
@@ -0,0 +1,20 @@
+//
+//  URLRequestFactory.swift
+//  BabylonHealthDemo
+//
+//  Created by Michail Zarmakoupis on 30/11/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import Foundation
+
+final class URLRequestFactory {
+    private static let baseURLString = "http://jsonplaceholder.typicode.com"
+    private static let postsPath = "/posts"
+    
+    static func postsRequest() -> URLRequest {
+        var components = URLComponents(string: baseURLString)!
+        components.path = postsPath
+        return URLRequest(url: components.url!)
+    }
+}

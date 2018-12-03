0b4192b79926f1b805dd7c5f847d2d3c5ee91423
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-12-03 13:00:59 +0200

    Add update method to CoreDataRepository

--- BabylonHealthDemo/CoreDataRepository.swift
+++ BabylonHealthDemo/CoreDataRepository.swift
@@ -27,11 +27,54 @@ final class CoreDataRepository: Repository {
             completion(.success(CoreDataRepository.localUsers(from: users)))
         } catch {
             print("Error fetching data from context \(error)")
+            completion(.error(error))
         }
     }
     
     func update(_ users: [LocalUser]) {
+        let context = persistentContainer.viewContext
+        clearUsers(context)
+        save(users, context: context)
+    }
+    
+    private func clearUsers(_ context: NSManagedObjectContext) {
+        let request: NSFetchRequest<CoreDataUser> = CoreDataUser.fetchRequest()
         
+        do {
+            let coreDataUsers = try context.fetch(request)
+            coreDataUsers.forEach { context.delete($0) }
+            
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
+    private func save(_ users: [LocalUser], context: NSManagedObjectContext) {
+        users.forEach { user in
+            let coreDataUser = CoreDataUser(context: context)
+            coreDataUser.id = Int64(user.id)
+            coreDataUser.name = user.name
+            coreDataUser.username = user.username
+            user.posts.forEach { post in
+                let coreDataPost = CoreDataPost(context: context)
+                coreDataPost.id = Int64(post.id)
+                coreDataPost.title = post.title
+                coreDataPost.body = post.body
+                coreDataPost.user = coreDataUser
+                post.comments.forEach { comment in
+                    let coreDataComment = CoreDataComment(context: context)
+                    coreDataComment.id = Int64(comment.id)
+                    coreDataComment.name = comment.name
+                    coreDataComment.body = comment.body
+                    coreDataComment.post = coreDataPost
+                }
+            }
+        }
+        do {
+            try context.save()
+        } catch {
+            print("Error saving data \(error)")
+        }
     }
 }
 

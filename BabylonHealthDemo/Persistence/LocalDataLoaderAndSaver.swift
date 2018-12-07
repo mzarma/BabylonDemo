9607afe10c5b30dff7a6ea7360d6ff6167fba481
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-12-07 17:53:33 +0200

    Organize project into folders

--- BabylonHealthDemo/Persistence/LocalDataLoaderAndSaver.swift
+++ BabylonHealthDemo/Persistence/LocalDataLoaderAndSaver.swift
@@ -94,4 +94,3 @@ private extension LocalDataLoaderAndSaver {
         }
     }
 }
-
--- BabylonHealthDemo/UI/Shared/UIColorHelpers.swift
+++ BabylonHealthDemo/UI/Shared/UIColorHelpers.swift
@@ -12,12 +12,4 @@ extension UIColor {
     static var customPurple: UIColor {
         return UIColor(red: 65.0 / 255.0, green: 3.0 / 255.0, blue: 153.0 / 255.0, alpha:1.0)
     }
-    
-    static var customPink: UIColor {
-        return UIColor(red: 224.0 / 255.0, green: 28.0 / 255.0, blue: 172.0 / 255.0, alpha:1.0)
-    }
-
-    static var customBlue: UIColor {
-        return UIColor(red: 31.0 / 255.0, green: 175.0 / 255.0, blue: 224.0 / 202.0, alpha:1.0)
-    }
 }
